520ddd2463367c3e7aea618b027e288253c41cfd
Radek <radek511@op.pl>
2018-11-25 02:34:05 +0000

    Add - Basic Node.js & Express ES6+ boilerplate

---
+++ back-end/src/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import { join } from 'path';
+import bodyParser from 'body-parser';
+import register from 'babel-core/register';
+import babelPolyfill from 'babel-polyfill';
+
+// Connect to database
+import dbConfig from './config/database';
+import mongoose from 'mongoose';
+
+mongoose.connect(dbConfig.mongoUrl);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (err) => {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// routes config
+app.get('/', (req, res) => {
+  res.send(`I'm handsome JS developer`);
+});
+
+
+// Start
+app.listen(4000, () => {
+    console.log(`Server is up!`);
+});
